b294d8483b0a2a9c28f144c4c942bfb2c8eb3569
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-15 20:25:18 +0100

    Add files via upload

---
+++ ControllerconQueryParameter/src/main/java/com/controllerconqueryparameter/ControllerconQueryParameter/ControllerconQueryParameterApplication.java
@@ -0,0 +1,13 @@
+package com.controllerconqueryparameter.ControllerconQueryParameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerconQueryParameterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerconQueryParameterApplication.class, args);
+	}
+
+}
---
+++ ControllerconQueryParameter/src/test/java/com/controllerconqueryparameter/ControllerconQueryParameter/ControllerconQueryParameterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controllerconqueryparameter.ControllerconQueryParameter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerconQueryParameterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
